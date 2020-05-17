15b6a809cf7d845d2571f6687fd9d0734c33f63c
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2020-05-17 17:04:09 +0900

    add routes

--- run.py
+++ run.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def hello_world():
     return "Hello World!"
 
+@app.route('/flask')
+def hello_sample():
+    return "Hello Flask."
+
+@app.route('/user/<user_id>')
+def hello_person(user_id):
+    return "Hello " + user_id
+
 if __name__ == '__main__':
     app.run()
